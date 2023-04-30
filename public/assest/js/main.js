782cc17bbd73abebfc4365f579b9d3e172748eb1
Prahla-Pawar <pawarprahlad1999@gmail.com>
2023-05-01 00:56:58 +0530

    last_push_done

--- public/assest/js/main.js
+++ public/assest/js/main.js
@@ -11,6 +11,30 @@ function removeClass(IdName, className){
   element.classList.remove(className);
 }
 
+function commercialBox(IdName, className){
+  const boxes = document.getElementsByClassName('commercialBox');
+  for (const box of boxes) {
+    box.classList.add('hideBox');
+  }
+  var element = document.querySelector(IdName);
+  element.classList.remove(className);
+}
+
+// $(document).ready(function(){
+//   var boxWidth = $(".commercialBox").width();
+//   $(".commercialBox").animate({width: 0});
+//   $(".clickBox").click(function(){
+//     $(".commercialBox").animate({
+//       width: 0
+//     });
+//   });
+//   $("#box3").click(function(){
+//     $(".commercialBox").animate({
+//       width: boxWidth
+//     });
+//   });
+// });
+
 // Animation
 AOS.init({
   duration: 800,
